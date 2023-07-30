947d0cca3816db45abf242dd03f36a0d7ce39e00
Mike <boxingbat>
2023-07-30 16:12:04 +0800

    add register-condition

--- routes/modules/users.js
+++ routes/modules/users.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const router = express.Router()
+const User = require('../../models/user')
 
 router.get('/login', (req, res) => {
     res.render('login')
@@ -13,8 +14,29 @@ router.get('/register', (req, res) => {
     res.render('register')
 })
 
-router.post('/register', (req, res) => {
-
+router.post('/register', async (req, res) => {
+    try {
+        const { name, email, password, confirmPassword } = req.body;
+        const user = await User.findOne({ email })
+        if (user) {
+            console.log('User already exists.')
+            return res.render('register', {
+                name,
+                email,
+                password,
+                confirmPassword
+            })
+        }
+        await User.create({
+            name,
+            email,
+            password
+        })
+        return res.redirect('/')
+    } catch (error) {
+        console.log(error);
+        return res.status(500).send('Server Error');
+    }
 })
 
 module.exports = router
