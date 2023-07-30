39418f9ba0a8497c5f5a53daa27de3d13dfad412
Mike <boxingbat>
2023-07-30 16:01:53 +0800

    add user.js , update seeder

--- models/records.js
+++ models/records.js
@@ -14,5 +14,16 @@ const recordSchema = new Schema({
         min: 0,
         required: true
     },
+    userId: {
+        type: Schema.Types.ObjectId,
+        ref: 'User',
+        index: true,
+        required: true
+    },
+    categoryId: {
+        type: Schema.Types.ObjectId,
+        ref: "Category",
+        required: true
+    }
 })
 module.exports = mongoose.model('Record', recordSchema)
